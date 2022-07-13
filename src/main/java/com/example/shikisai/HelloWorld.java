6f21e01a87d79608bea44aa3d5289b3ee4f9d057
hiroto-hash <hiroto5118@icloud.com>
2022-07-13 20:17:36 +0900

    firstCommit

---
+++ src/main/java/com/example/shikisai/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.shikisai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorld {
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("message", "Hello Springboot");
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/shikisai/ShikisaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shikisai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShikisaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShikisaiApplication.class, args);
+	}
+
+}
