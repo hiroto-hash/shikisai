16299af717c101df94018eb509137fb11119456b
hiroto-hash <hiroto5118@icloud.com>
2022-08-23 19:55:28 +0900

    html修正

---
+++ src/main/java/com/example/shikisai/article/controller/articleContller.java
@@ -0,0 +1,15 @@
+package com.example.shikisai.article.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class articleContller {
+  @RequestMapping(value = "/article", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("message", "Hello Springboot");
+        return "article/article";
+    }
+}
---
+++ src/main/java/com/example/shikisai/article/service/articleService.java
@@ -0,0 +1,5 @@
+package com.example.shikisai.article.service;
+
+public class articleService {
+
+}
